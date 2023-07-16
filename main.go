95b4c5220aba3d116534fbfeab5d54ac29a8fb34
Jose Berrio <aldarisbm@gmail.com>
2023-07-16 19:43:07 -0400

    not very good git commit comment

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"github.com/aldarisbm/seratobackup/watcher"
+)
 
+func main() {
+	folderWatcher := watcher.NewFolderWatcher(
+		"/Users/berrio/Development/seratobackup/test_folder",
+		true,
+		true,
+	)
+
+	folderWatcher.StartWatcher()
 }
--- watcher/folder_watcher.go
+++ watcher/folder_watcher.go
@@ -1,1 +1,30 @@
 package watcher
+
+import "time"
+
+type FolderWatcher struct {
+	path               string
+	includeHiddenFiles bool
+	includeSubfolders  bool
+	interval           time.Duration
+}
+
+func NewFolderWatcher(path string, hiddenFiles, subfolders bool) *FolderWatcher {
+	return &FolderWatcher{
+		path:               path,
+		includeHiddenFiles: hiddenFiles,
+		includeSubfolders:  subfolders,
+		interval:           1 * time.Second,
+	}
+}
+
+func (f *FolderWatcher) StartWatcher() {
+	for {
+		f.watch()
+		time.Sleep(f.interval)
+	}
+}
+
+func (f *FolderWatcher) watch() {
+
+}
